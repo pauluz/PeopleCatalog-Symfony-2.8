0d9797adb6e4cd907bb4c2165e4d59ffdf615002
pauluZ <pawel.szczepanek@gmail.com>
2017-07-15 00:35:23 +0200

    dodanie ponad 900 miast do Fixtures

--- src/AppBundle/DataFixtures/ORM/data/cat_person-localhost.php
+++ src/AppBundle/DataFixtures/ORM/data/cat_person-localhost.php
@@ -10,7 +10,7 @@
 
 /* `create_people`.`cat_person` */
 $cat_person = array(
-  array('id' => '1','cat_city_id' => '2','cat_company_office_id' => '3','firstname' => 'Marek','lastname' => 'Karkosik','birth' => '1979-06-11'),
-  array('id' => '2','cat_city_id' => '1','cat_company_office_id' => '13','firstname' => 'Joanna','lastname' => 'Kwiatkowska','birth' => '1984-09-24'),
-  array('id' => '3','cat_city_id' => '2','cat_company_office_id' => '7','firstname' => 'Kuba','lastname' => 'Wiśniewski','birth' => '1980-11-30')
+  array('id' => '1','cat_city_id' => '68','cat_company_office_id' => '3','firstname' => 'Marek','lastname' => 'Karkosik','birth' => '1979-06-11'),
+  array('id' => '2','cat_city_id' => '729','cat_company_office_id' => '13','firstname' => 'Joanna','lastname' => 'Kwiatkowska','birth' => '1984-09-24'),
+  array('id' => '3','cat_city_id' => '761','cat_company_office_id' => '7','firstname' => 'Kuba','lastname' => 'Wiśniewski','birth' => '1980-11-30')
 );
